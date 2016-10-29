422f2c12a48d8331168ebdcafbca9f50b7fcd0aa
Animesh Sinha <sinha.animesh34@gmail.com>
2016-10-29 17:40:43 -0400

    added method to get inserted words

--- src/vandalism_detection/features/edit.py
+++ src/vandalism_detection/features/edit.py
@@ -7,3 +7,9 @@ def diff_inserted_text(old_text, new_text):
     inserted_text = "".join(additions)
     inserted_text = inserted_text.replace(" ", "")
     return inserted_text
+
+def diff_inserted_words(old_text, new_text):
+    diff_object = diff_match_patch()
+    diff = diff_object.diff_main(old_text, new_text)
+    inserted_words = [item[1] for item in diff if item[0] == 1]
+    return inserted_words

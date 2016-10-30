51d884f6847856638134cba5a1f708bb72aa4452
Animesh Sinha <sinha.animesh34@gmail.com>
2016-10-30 13:27:46 -0400

    added biased words feature

--- src/vandalism_detection/features/word_categories.py
+++ src/vandalism_detection/features/word_categories.py
@@ -2,7 +2,7 @@ import csv
 from search_file import find
 from create_csv import initialize, build_csv
 from edit import diff_inserted_words
-from word_list import vulgar_words
+from word_list import vulgar_words, biased_words
 
 def calculate_vulgarism_ratio(inserted_words):
     vulgar_word_list = vulgar_words()
@@ -11,6 +11,20 @@ def calculate_vulgarism_ratio(inserted_words):
         if word in vulgar_word_list:
             vulgar_words_count = vulgar_words_count + 1
     return 0.0 if not inserted_words else ((float)(vulgar_words_count) / len(inserted_words))
+
+def calculate_biased_ratio(inserted_words):
+    biased_word_list = biased_words()
+    biased_words_count = 0
+    for word in inserted_words:
+        if word in biased_word_list:
+            biased_words_count = biased_words_count + 1
+    return 0.0 if not inserted_words else ((float)(biased_words_count) / len(inserted_words))
+
+def calculate_word_categories_ratios(inserted_words):
+    vulgarism_ratio = calculate_vulgarism_ratio(inserted_words)
+    biased_ratio = calculate_biased_ratio(inserted_words)
+    ratios = str(vulgarism_ratio) + ',' + str(biased_ratio)
+    return ratios
 
 if __name__ == "__main__":
     values, T = initialize()
@@ -24,8 +38,8 @@ if __name__ == "__main__":
             old_text = open(old_revision_path,'r').read()
             new_text = open(new_revision_path,'r').read()
             inserted_words = diff_inserted_words(old_text, new_text)
-            vulgarism_ratio = calculate_vulgarism_ratio(inserted_words)
-            values.append(vulgarism_ratio)
+            ratios = calculate_word_categories_ratios(inserted_words)
+            values.append(ratios)
             T = T - 1
             if T == 0:
                 break
--- src/vandalism_detection/features/word_list.py
+++ src/vandalism_detection/features/word_list.py
@@ -58,3 +58,14 @@ def vulgar_words():
     "homoey","queero","freex","cunthunter","shamedame","slutkiss","shiteater","fuckass","fucka$$","clitorus","assfucker",
     "assfuckers","dillweed","cracker","teabagging","shitt","azz","fuk","fucknugget","cuntlick","g@y","@ss","beotch","pussys",
     "s***","paedophile","pedophiles","pedophile","sucks","licker","lickers","bitchface","idiot","tosser","idiots","tossers"}
+
+def biased_words():
+    return {"acclaimed","amazing","astonishing","authoritative","beautiful","best","brilliant","canonical","celebrated",
+    "charismatic","classic","cutting-edge","defining","definitive","eminent","enigma","exciting","extraordinary","fabulous",
+    "famous","infamous","fantastic","fully","genius","global","great","greatest","iconic","immensely","impactful",
+    "incendiary","indisputable","influential","innovative","inspired","intriguing","leader","leading","legendary","major",
+    "masterly","mature","memorable","notable","outstanding","pioneer","popular","prestigious","really","remarkable","renowned",
+    "respected","seminal","significant","skillful","solution","single-handedly","staunch","talented","most","top","transcendent",
+    "undoubtedly","unique","visionary","virtually","virtuoso","well-known","well-established","world-class","worst","coolest",
+    "super","probably","hate","ugly","fat","lame","weird","strange","everyone","cares","boring","boreing","ever","huge","like",
+    "idiotic","absolute","total","totally"}

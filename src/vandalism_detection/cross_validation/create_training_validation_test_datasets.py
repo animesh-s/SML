913bf7f2a0a73e40aed8012105a723c13dc5ac4c
Animesh Sinha <sinha.animesh34@gmail.com>
2016-11-02 19:49:57 -0400

    refactoring

--- src/vandalism_detection/cross_validation/create_training_validation_test_datasets.py
+++ src/vandalism_detection/cross_validation/create_training_validation_test_datasets.py
@@ -1,6 +1,32 @@
 import csv
 import random
 
+def reservoir_sampling(indices, training_count, validation_count, test_count):
+    training_samples = random.sample(indices, training_count)
+    training_samples.sort()
+    indices = [index for index in indices if index not in training_samples]
+    validation_samples = random.sample(indices, validation_count)
+    validation_samples.sort()
+    indices = [index for index in indices if index not in training_samples]
+    test_samples = random.sample(indices, test_count)
+    test_samples.sort()
+    return training_samples, validation_samples, test_samples
+
+def create_dataset(samples, training_samples, validation_samples, test_samples):
+    writer = open('../../../../training_set.csv', 'w')
+    for index in training_samples:
+        writer.write(samples[index] + '\n')
+    writer.close()
+    writer = open('../../../../validation_set.csv', 'w')
+    for index in validation_samples:
+        writer.write(samples[index] + '\n')
+    writer.close()
+    writer = open('../../../../test_set.csv', 'w')
+    for index in test_samples:
+        writer.write(samples[index] + '\n')
+    writer.close()
+    print 'Data splitting done.\nTraining Set - 60%\nValidation Set - 20%\nTest Set - 20%'
+
 if __name__ == "__main__":
     T = 32439
     training_count = (int)(0.6 * T)
@@ -15,27 +41,6 @@ if __name__ == "__main__":
         indices.append(count)
         samples.append(','.join(row))
         count = count + 1
-
-    training_samples = random.sample(indices, training_count)
-    training_samples.sort()
-    indices = [index for index in indices if index not in training_samples]
-    validation_samples = random.sample(indices, validation_count)
-    validation_samples.sort()
-    indices = [index for index in indices if index not in training_samples]
-    test_samples = random.sample(indices, test_count)
-    test_samples.sort()
-
-    writer = open('../../../../training_set.csv', 'w')
-    for index in training_samples:
-        writer.write(samples[index] + '\n')
-    writer.close()
-    writer = open('../../../../validation_set.csv', 'w')
-    for index in validation_samples:
-        writer.write(samples[index] + '\n')
-    writer.close()
-    writer = open('../../../../test_set.csv', 'w')
-    for index in test_samples:
-        writer.write(samples[index] + '\n')
-    writer.close()
-    print 'Data splitting done.\nTraining Set - 60%\nValidation Set - 20%\nTest Set - 20%'
-    
+        
+    training_samples, validation_samples, test_samples = reservoir_sampling(indices, training_count, validation_count, test_count)
+    create_dataset(samples, training_samples, validation_samples, test_samples)

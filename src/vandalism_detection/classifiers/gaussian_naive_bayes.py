b9b0cbb7344077f265e0d411d85de34218f4cf00
Animesh Sinha <sinha.animesh34@gmail.com>
2016-11-03 03:49:09 -0400

    called naive bayes on equal datasets

--- src/vandalism_detection/classifiers/gaussian_naive_bayes.py
+++ src/vandalism_detection/classifiers/gaussian_naive_bayes.py
@@ -6,12 +6,14 @@ from sklearn.externals import joblib
 from sklearn import preprocessing
 from metrics import calculate_metrics
 
-if __name__ == "__main__":
-    training_samples = genfromtxt('../../../../training_set.csv', delimiter=',', usecols = (1,2,3,9,11))
-    training_labels = genfromtxt('../../../../training_set.csv', delimiter=',', usecols = range(17,18) , dtype=None)
-    validation_samples = genfromtxt('../../../../validation_set.csv', delimiter=',', usecols = (1,2,3,9,11))
-    validation_labels = genfromtxt('../../../../validation_set.csv', delimiter=',', usecols = range(17,18), dtype=None)
-    clf = GaussianNB()
+def samples_and_labels(count):
+    training_samples = genfromtxt('../../../../training_set_' + str(count) + '.csv', delimiter=',', usecols = (1,2,3,9,11))
+    training_labels = genfromtxt('../../../../training_set_' + str(count) + '.csv', delimiter=',', usecols = range(17,18) , dtype=None)
+    validation_samples = genfromtxt('../../../../validation_set_' + str(count) + '.csv', delimiter=',', usecols = (1,2,3,9,11))
+    validation_labels = genfromtxt('../../../../validation_set_' + str(count) + '.csv', delimiter=',', usecols = range(17,18), dtype=None)
+    return training_samples, training_labels, validation_samples, validation_labels
+
+def fit_and_predict(clf, training_samples, training_labels, validation_samples, validation_labels):
     clf.fit(training_samples, training_labels)
     result = clf.predict(validation_samples)
     true_positive = 0
@@ -26,6 +28,15 @@ if __name__ == "__main__":
         elif validation_labels[sample]==1 and predicted_label==0:
             false_negative = false_negative + 1
         else:
-            true_positive = true_positive + 1     
+            true_positive = true_positive + 1
     accuracy, precision, recall = calculate_metrics(true_positive, true_negative, false_positive, false_negative)
-    print 'Accuracy = ' + str(accuracy) + '\nPrecision = ' + str(precision) + '\nRecall = ' + str(recall)+ '\nPrecision = ' + str(precision) + '\n'
+    print 'Accuracy = ' + str(accuracy) + '\nPrecision = ' + str(precision) + '\nRecall = ' + str(recall) + '\n'
+
+def run_Gaussian_Naive_Bayes(clf, count):
+    training_samples, training_labels, validation_samples, validation_labels = samples_and_labels(count)
+    fit_and_predict(clf, training_samples, training_labels, validation_samples, validation_labels)
+
+if __name__ == "__main__":
+    clf = GaussianNB()
+    run_Gaussian_Naive_Bayes(clf, 700)
+    run_Gaussian_Naive_Bayes(clf, 2394)

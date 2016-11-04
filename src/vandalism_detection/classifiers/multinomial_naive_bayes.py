123d5d7b1a28ba39b2a2583d892dbad48230231f
Animesh Sinha <sinha.animesh34@gmail.com>
2016-11-04 00:18:28 -0400

    created txt files for plots

--- src/vandalism_detection/classifiers/multinomial_naive_bayes.py
+++ src/vandalism_detection/classifiers/multinomial_naive_bayes.py
@@ -3,23 +3,26 @@ import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 from sklearn import preprocessing
-from metrics import calculate_precision_recall, samples_and_labels
+from metrics import calculate_precision_recall, samples_and_labels, create_result_txt_for_multinomial_naive_bayes
 
-def fit_and_predict(clf, alpha, training_samples, training_labels, validation_samples, validation_labels):
+def fit_and_predict(clf, alpha, count, training_samples, training_labels, validation_samples, validation_labels, use_balanced_set, use_feature_selection):
     clf.fit(training_samples, training_labels)
     result = clf.predict(validation_samples)
     accuracy, precision, recall, f_score = calculate_precision_recall(validation_labels, result)
-    print 'Alpha = ' + str(alpha) + '\nAccuracy = ' + str(accuracy) + '\nPrecision = ' + str(precision) + '\nRecall = ' + str(recall) + '\nF Score = ' + str(f_score) + '\n'
+    create_result_txt_for_multinomial_naive_bayes(count, alpha, accuracy, precision, recall, f_score, use_balanced_set, use_feature_selection)
 
-def run_Multinomial_Naive_Bayes(clf, alpha, count):
-    training_samples, training_labels, validation_samples, validation_labels = samples_and_labels(count, True)
-    fit_and_predict(clf, alpha, training_samples, training_labels, validation_samples, validation_labels)
+def run_Multinomial_Naive_Bayes(clf, alpha, count, use_balanced_set, use_feature_selection):
+    training_samples, training_labels, validation_samples, validation_labels = samples_and_labels(count, use_balanced_set, use_feature_selection, True)
+    fit_and_predict(clf, alpha, count, training_samples, training_labels, validation_samples, validation_labels, use_balanced_set, use_feature_selection)
 
 if __name__ == "__main__":
-    alpha_values = [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007, 0.0008, 0.0009, 0.001, 0.002, 0.003, 0.004, 
-                    0.005, 0.006, 0.007, 0.008, 0.009, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.2, 
-                    0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 50.0, 100.0]
+    alpha_values = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0]
     for alpha in alpha_values:
+        print 'Running for alpha = ' + str(alpha) + '\n'
         clf = MultinomialNB(alpha=alpha)
-        run_Multinomial_Naive_Bayes(clf, alpha, 700)
-        run_Multinomial_Naive_Bayes(clf, alpha, 2394)
+        run_Multinomial_Naive_Bayes(clf, alpha, 700, True, True)
+        run_Multinomial_Naive_Bayes(clf, alpha, 700, True, False)
+        run_Multinomial_Naive_Bayes(clf, alpha, 2394, True, True)
+        run_Multinomial_Naive_Bayes(clf, alpha, 2394, True, False)
+        run_Multinomial_Naive_Bayes(clf, alpha, 0, False, True)
+        run_Multinomial_Naive_Bayes(clf, alpha, 0, False, False)
